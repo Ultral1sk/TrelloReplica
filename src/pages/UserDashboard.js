440ca9e27ffe194ec4ba53d25678002f0e9fb2c3
Ultral1sk <jderebanoski@gmail.com>
2020-06-17 23:11:50 +0200

    Register and Login data controlled and ready to be received to backend

---
+++ src/pages/UserDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDashboard = () => {
+      return (
+            <div>
+                  Hello from Dashboard
+            </div>
+      )
+}
+
+export default UserDashboard
